ea7876c258c82bcd54a33b4ec4bdee9e3222f427
mi231208 <50330970+mi231208@users.noreply.github.com>
2025-04-04 13:14:59 +0700

    main.py

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ print("===========================================\n")
 time.sleep(1)
 
 channel_id = input("Masukkan ID channel: ")
-waktu1 = int(input("Set Waktu Hapus Pesan: "))
 waktu2 = int(input("Set Waktu Kirim Pesan: "))
 
 time.sleep(1)
@@ -46,37 +45,18 @@ with open("token.txt", "r") as f:
     authorization = f.readline().strip()
 
 while True:
-        channel_id = channel_id.strip()
+    channel_id = channel_id.strip()
 
-        payload = {
-            'content': random.choice(words).strip()
-        }
+    payload = {
+        'content': random.choice(words).strip()
+    }
 
-        headers = {
-            'Authorization': authorization
-        }
+    headers = {
+        'Authorization': authorization
+    }
 
-        r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
-        print(Fore.WHITE + "Sent message: ")
-        print(Fore.YELLOW + payload['content'])
+    r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
+    print(Fore.WHITE + "Sent message: ")
+    print(Fore.YELLOW + payload['content'])
 
-        response = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
-
-        if response.status_code == 200:
-            messages = response.json()
-            if len(messages) == 0:
-                is_running = False
-                break
-            else:
-                time.sleep(waktu1)
-
-                message_id = messages[0]['id']
-                response = requests.delete(f'https://discord.com/api/v9/channels/{channel_id}/messages/{message_id}', headers=headers)
-                if response.status_code == 204:
-                    print(Fore.GREEN + f'Pesan dengan ID {message_id} berhasil dihapus')
-                else:
-                    print(Fore.RED + f'Gagal menghapus pesan dengan ID {message_id}: {response.status_code}')
-        else:
-            print(f'Gagal mendapatkan pesan di channel: {response.status_code}')
-
-        time.sleep(waktu2)
+    time.sleep(waktu2)
